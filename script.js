341a8957f6df063819ded9e694a5930b112b1e3c
Rene Angelo de los Reyes <21-1-01027@vsu.edu.ph>
2024-04-14 14:11:17 +0800

    [2024-04-14] Everything is a disaster
    * Have to rework the firebase upload feature

--- script.js
+++ script.js
@@ -37,3 +37,54 @@ function logout() {
   // Perform logout actions here
   window.location.href = "admin.html"; // Example: Redirect to logout page
 }
+
+function getFile(e) {}
+
+/* function getImagePreview(event) {
+  var image = URL.createObjectURL(event.target.files[0]);
+  var imageDiv = document.getElementById("card");
+  var newImg = document.createElement("img");
+  imageDiv.innerHTML = ""; // Clear the contents of the imageDiv
+  newImg.src = image;
+  newImg.width = "200";
+  newImg.height = "200";
+  imageDiv.appendChild(newImg);
+
+  // Change the color of the fa-circle-plus icon to white
+  var circlePlusIcon = document.querySelector(".fa-circle-plus");
+  circlePlusIcon.classList.add("white-icon");
+} */
+
+/* var form = document.getElementById("myForm"),
+  imgInput = document.querySelector(".img"),
+  file = document.getElementById("imgInput"),
+  identification = document.getElementById("identification"),
+  name = document.getElementById("name"),
+  price = document.getElementById("price"),
+  description = document.getElementById("description"),
+  seller = document.getElementById("seller"),
+  submitBtn = document.querySelector(".submit"),
+  userInfo = document.getElementById("data");
+
+let getData = localStorage.getItem("imgInput")
+  /? JSON.parse(localStorage.getItem("imgInput"))
+  : [];
+
+let isData = false,
+  editId;
+
+file.onchange = function () {
+  console.log("File selected");
+  if (file.files && file.files[0]) {
+    if (file.files[0].size < 1000000) {
+      var fileReader = new FileReader();
+      fileReader.onload = function (e) {
+        imgUrl = e.target.result;
+        imgInput.src = imgUrl;
+      };
+      fileReader.readAsDataURL(file.files[0]);
+    } else {
+      alert("This file is too large!");
+    }
+  }
+}; */
